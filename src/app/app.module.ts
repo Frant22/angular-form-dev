5886a794f7dec4e69190bf8ff2fdeafdb2aedace
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-04-19 13:47:52 +0300

    70% validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CandidateFormComponent } from './candidate-form/candidate-form.component';
+
 
 @NgModule({
   declarations: [
@@ -15,9 +21,14 @@ import { CandidateFormComponent } from './candidate-form/candidate-form.componen
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    // MatFormFieldModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/candidate-form/candidate-form.component.ts
+++ src/app/candidate-form/candidate-form.component.ts
@@ -1,34 +1,35 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-candidate-form',
   templateUrl: './candidate-form.component.html',
   styleUrls: ['./candidate-form.component.scss']
 })
+
 export class CandidateFormComponent {
 
-  technologies = {
+  technologies: any = {
     Angular: ['1.1.1', '1.2.1', '1.3.3'],
     React: ['2.1.2', '3.2.4', '4.3.1'],
     Vue: ['3.3.1', '5.2.1', '5.1.3']
   }
 
   hasSubmitted = false;
+  isSelected = true;
 
   selectedFramework!: string;
- 
+  
+
   candidateForm = new FormGroup({
-    firstName: new FormControl(),
+    firstName: new FormControl('', Validators.required),
     lastName: new FormControl(),
-    dateOfBirth: new FormControl(),
-    jsTechnology: new FormControl(),
+    dateOfBirth: new FormControl('', Validators.required),
+    jsTechnology: new FormControl('', Validators.required),
     technologyVersion: new FormControl(),
     email: new FormControl(),
-    hobby: new FormGroup({
-      nameOfHobby: new FormControl(),
-      hobbyExpirience: new FormControl()
-    })
+    nameOfHobby: new FormControl(),
+    hobbyExpirience: new FormControl()
   });
 
   onSubmit(){
@@ -36,12 +37,29 @@ export class CandidateFormComponent {
     console.log(this.candidateForm.value);
   }
 
-  onChange(framework: string) {
-    this.selectedFramework = framework;
-    console.log(this.selectedFramework);
+  onFrameworkChange(event: any) {
+    this.selectedFramework = event.target.value;
+  }
+
+  get _firstName() {
+    return this.candidateForm.get('firstName');
+  }
+
+  get _lastName() {
+    return this.candidateForm.get('lastName');
+  }
+
+  get _dateOfBirth() {
+    return this.candidateForm.get('dateOfBirth');
+  }
+
+  get _jsTechnology() {
+    return this.candidateForm.get('jsTechnology');
+  }
+
+  get _technologyVersion() {
+    return this.candidateForm.get('technologyVersion');
   }
 }
-
-
 
 
